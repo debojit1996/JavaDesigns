75f62f878868a9ca47518891332f4eddc4c7b685
debojit.chakraborty <debojit.chakraborty@philips.com>
2021-06-28 22:57:30 +0530

    Added notes for builder pattern

--- BuilderPattern/src/main/java/Shop.java
+++ BuilderPattern/src/main/java/Shop.java
@@ -1,5 +1,19 @@
 public class Shop {
     public static void main(String[] args) {
+        // Traditional way of creating a Phone object as below:
+        /* Problems here:
+            1. We need to use the exact same order unless we declare different constructors accepting
+            args in different order.
+            2. We always will have to pass all the values in the constructor( again unless we do some
+            constructor overloading)
+         */
+        Phone newPhone = new Phone("iOS", 8, "MediaTek", 4000, 5.5);
+        System.out.println("Traditional phone: "+newPhone);
+
+        /*
+          With this builder pattern, we can only set the variables we want to set. Also, the order of
+          setting these variables doesn't matter
+         */
         Phone phone = new PhoneBuilder()
                 .setOs("Android")
                 .setProcessor("Qualcomm Snapdragon 765G")
